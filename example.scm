0385bbe9d98369cd55f77d69367d622d3f935597
Sam Vervaeck <samvv@pm.me>
2026-03-19 21:37:35 +0100

    Extend example file with more theorems and some tests

--- example.scm
+++ example.scm
@@ -1,22 +1,76 @@
-;; These are some tests for the tabulator
+;; These are some tests
 ;; All of these formulas should be reported as being a tautology
+
+; Double negation
+(defthm neg-neg
+  (equiv
+    a
+    (not (not a)))
+  #:tactic tabulate)
+
+; De Morgan's law for OR
+(defthm de-morgan-and
+  (equiv
+    (not (and a b))
+    (or (not a) (not b)))
+  #:tactic tabulate)
+
+; De Morgan's law for AND
+(defthm de-morgan-and
+  (equiv
+    (not (or a b))
+    (and (not a) (not b)))
+  #:tactic tabulate)
+
+; Commutativity of AND
+(defthm comm-and
+  (equiv
+    (and a b)
+    (and b a))
+  #:tactic tabulate)
+
+; Commutativity of OR
+(defthm comm-or
+  (equiv
+    (or a b)
+    (or b a))
+  #:tactic tabulate)
 
 ; If either a implies c or b implies c (or both), then a and b, taken together, surely must imply c.
 ; If we can infer both a and b from c, we can either imply c from a or c from b (or both).
-(equiv
-  (or
-    (implies a c)
-    (implies b c))
-  (implies
-    (and a b)
-    c))
+(defthm disj-implies-conj
+  (equiv
+    (or
+      (implies a c)
+      (implies b c))
+    (implies
+      (and a b)
+      c))
+  #:tactic tabulate)
 
 ; If from c we can imply a, and from c we can imply b, we can imply both a and b from c alone.
 ; If we can infer a as well as b from c, we can separately imply a from c and b from c.
-(equiv
-  (and
-    (implies c a)
-    (implies c b))
-  (implies
-    c
-    (and a b)))
+(defthm conj-implies-conj
+  (equiv
+    (and
+      (implies c a)
+      (implies c b))
+    (implies
+      c
+      (and a b)))
+  #:tactic tabulate)
+
+(defthm test-0
+  (equiv
+    (and (not (or d c)) k)
+    (and (not (or d c)) k)))
+
+(defthm test-1
+  (equiv
+    (and (not (or d c)) k)
+    (and (and (not (not (not d))) (not c)) k)))
+
+(defthm test-2
+  (equiv
+    (or a b)
+    (not (not (or a b)))))
